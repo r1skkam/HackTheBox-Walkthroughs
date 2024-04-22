73a31d5d8109d454a1af03f25deafa861a539b3f
Kam Khan Sang <58542375+r1skkam@users.noreply.github.com>
2024-04-22 17:00:14 +0630

    Create index.php

---
+++ After failed OSCP/UpDown/index.php
@@ -0,0 +1,12 @@
+<b>This is only for developers</b>
+<br>
+<a href="?page=admin">Admin Panel</a>
+<?php
+	define("DIRECTACCESS",false);
+	$page=$_GET['page'];
+	if($page && !preg_match("/bin|usr|home|var|etc/i",$page)){
+		include($_GET['page'] . ".php");
+	}else{
+		include("checker.php");
+	}	
+?>
